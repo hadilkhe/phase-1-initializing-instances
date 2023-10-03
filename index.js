7d1a6192ef5920b9294a573bc846d682f26507c1
hadilkhe <121692875+hadilkhe@users.noreply.github.com>
2023-10-03 16:03:23 +0100

    finished

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your code here
+class Breakfast {
+    constructor(food, drink) {
+      this.food = food;
+      this.drink = drink;
+    }
+  }
+// Lunch 
+class Lunch {
+    constructor(salad, soup, drink) {
+      this.salad = salad;
+      this.soup = soup;
+      this.drink = drink;
+    }
+  }
+//dinner 
+class Dinner {
+    constructor(salad, soup, entree, dessert) {
+      this.salad = salad;
+      this.soup = soup;
+      this.entree = entree;
+      this.dessert = dessert;
+    }
+  }
+
+  const myBreakfast = new Breakfast("toast", "coffee");
+const myLunch = new Lunch("caesar salad", "tomato soup", "iced tea");
+const myDinner = new Dinner("garden salad", "butternut squash soup", "steak", "chocolate cake");
+
+console.log(myBreakfast); 
+console.log(myLunch);     
+console.log(myDinner);    
